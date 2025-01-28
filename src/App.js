586e96adfe59693f69157f8ae420e35c2199f528
Rohit Kumar <144544590+RohitTheCoderr@users.noreply.github.com>
2025-01-28 12:26:20 +0530

    check

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { createBrowserRouter, Outlet } from "react-router-dom";
+import { FaMoon } from "react-icons/fa";
+import { FiSun } from "react-icons/fi";
+import Home from "./components/Home/Home";
+import Contact from "./components/Contact/Contact";
+import About from "./components/About/About";
+import Blog from "./components/Blog/Blog";
+import Error from "./components/Error/Error";
+import Navbar from "./components/header/Navbar";
+import Ourclients from "./components/Ourclients/Ourclients";
+import { useState } from "react";
+import Footer from "./components/footer/Footer";
 function App() {
+  const [color, setColor] = useState("transparent");
+
+  function myfun() {
+    if (color === "transparent") {
+      setColor("#0f0f10");
+    } else {
+      setColor("transparent");
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="p-0 m-0" style={{ background: `${color}` }}>
+        <button className=" absolute z-30 top-4 sm:top-6 md:top-6 xl:top-10 left-[93.5%] sm:left-[95%] " onClick={myfun}>
+          {color==="transparent" ? <FaMoon className="text-[16px] sm:text-xl xl:text-2xl"/> :<FiSun className="text-yellow-400 text-[16px] sm:text-xl xl:text-2xl"/>
+          }
+        </button>
+        <Navbar />
+        <Outlet />
+        <Footer/>
+        {/* <Home/> */}
+        <footer/>
+      </div>
+    </>
   );
 }
 
+export const route = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/blog",
+        element: <Blog />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+      {
+        path: "/ourclients",
+        element: <Ourclients />,
+      },
+    ],
+    errorElement: <Error />,
+  },
+]);
 export default App;
